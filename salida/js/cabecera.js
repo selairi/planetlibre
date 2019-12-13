b8721b7ec1f3f7edb293c68bf0e8a61bd447b6b1
P.L. Lucas <selairi@gmail.com>
2019-12-13 23:28:52 +0100

    Recreado el sitio de PlanetLibre.

---
+++ salida/js/cabecera.js
@@ -0,0 +1,8 @@
+document.write(`<div class="navbar">
+<a href="index.html">Inicio</a>
+  <a href="https://github.com/selairi/planetlibre"target="_blank">Código en GitHub</a>
+  <a href="https://gitlab.com/victorhck/planetalibre"target="_blank">GitLab Pages</a>
+  <a href="RSS.html"target="_blank">Suscribete a los RSS de PlanetaLibre</a>
+  <a href="fuentes.html">Fuentes</a>
+  <a href="acerca.html">Acerca de...</a>
+</div>`);
---
+++ salida/js/cuadro.js
@@ -0,0 +1,3 @@
+document.write(`<div id="topRight">
+<img src="imagenes/dots_connected.png" alt="PlanetaLibre" style="width:240px;height:140px">
+</div>`);
