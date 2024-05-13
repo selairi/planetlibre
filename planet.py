05983e135f19592649cfc957354ab1e03ec9bce9
P. L. Lucas <selairi@gmail.com>
2024-05-13 08:44:49 +0200

    Se arregla el error al haber símbolos HTML en los RSS y se cambia el aspecto predeterminado.

--- planet.py
+++ planet.py
@@ -4,39 +4,38 @@
 #
 #   Copyright (C) 2019 P.L. Lucas
 #
+# LICENSE: BSD-3
 #
-# LICENSE: BSD
-# You may use this file under the terms of the BSD license as follows:
+# All rights reserved.
 #
-# "Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#   * Redistributions of source code must retain the above copyright
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions 
+# are met:
+#
+#    Redistributions of source code must retain the above copyright 
 #     notice, this list of conditions and the following disclaimer.
-#   * Redistributions in binary form must reproduce the above copyright
+#    Redistributions in binary form must reproduce the above copyright
 #     notice, this list of conditions and the following disclaimer in
-#     the documentation and/or other materials provided with the
+#     the documentation and/or other materials provided with the 
 #     distribution.
-#   * Neither the name of developers or companies in the above copyright, Digia Plc and its 
-#     Subsidiary(-ies) nor the names of its contributors may be used to 
-#     endorse or promote products derived from this software without 
-#     specific prior written permission.
+#    Neither the name of copyright holders nor the names of its 
+#     contributors may be used to endorse or promote products derived 
+#     from this software without specific prior written permission.
 #
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT
+# HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
 # LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+# DAMAGE.
 #
-#
-######################################
+#######################################
 
 import requests
 import feedparser
@@ -106,14 +105,15 @@ def generar_html(sql_conn):
         n += 1
         fecha = time.gmtime(int(row[3]))
         fout.write("""
-        <tr>
-            <td class='col_fecha'>{3}</td>
-            <td class='col_enlace'>{0}:<br><a href='{2}' target='blank'>{1}</a></td>
-        </tr>
+<div class='noticia'>
+ <div class='enlace'><a href='{2}' target='blank'>{1}</a></div>
+ <div class='blog'>{0}</div>
+ <div class='fecha'>{3}</div>
+</div>
         """.format(clean_html(row[0]), clean_html(row[1]), row[2], "{0}-{1}-{2}".format(fecha[0], format(fecha[1], "0>2"), format(fecha[2], "0>2"))))
         if n % 1000 == 0:
             pagina += 1
-            fout.write('</table>')
+            fout.write('</div>')
             archivo_siguiente = 'pagina-{0}.html'.format(pagina)
             fout.write("<p>")
             if archivo_anterior!= None:
@@ -125,7 +125,7 @@ def generar_html(sql_conn):
             fout.close()
             fout = open('salida/pagina-{0}.html'.format(pagina), 'w')
             cabecera_html(fout)
-    fout.write('</table>')
+    fout.write('</div>')
     if archivo_anterior!= None:
         fout.write("<p><a href='{0}'>Anterior</a></p>".format(archivo_anterior))
     pie_html(fout)
@@ -150,7 +150,7 @@ def generar_rss(sql_conn):
             <link>{enlace}</link>
             <description>{titulo}</description>
             <pubDate>{fecha}</pubDate>
-            </item>""".format(blog=row[0], titulo=row[1], enlace=clean_html(row[2]), fecha=time.asctime(fecha)))
+            </item>""".format(blog=clean_html(row[0]), titulo=clean_html(row[1]), enlace=clean_html(row[2]), fecha=clean_html(time.asctime(fecha))))
         if n > 10:
             break
     fout.write("</channel>\n</rss>")
